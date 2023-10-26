365733cee0e1cef58a3174d7783d89007c4d878f
ichbinallen <aclark494@gmail.com>
2023-10-26 15:13:05 -0700

    scrape all pages

--- rentpy/spiders/rent_spider.py
+++ rentpy/spiders/rent_spider.py
@@ -6,11 +6,17 @@ class RentSpider(scrapy.Spider):
     part1 = "htt" + "ps://w" + "ww.re"
     part2 = "dfi" + "n.com"
     base_url = part1 + part2
+    city_url = "/city/1387/WA/Bellevue"
     start_urls = [
-        f"{part1}{part2}/city/1387/WA/Bellevue",
+        f"{part1}{part2}{city_url}",
     ]
 
     def parse(self, response):
+        part1 = "htt" + "ps://w" + "ww.re"
+        part2 = "dfi" + "n.com"
+        base_url = part1 + part2
+        city_url = "/city/1387/WA/Bellevue"
+
         homes = response.css("div.HomeCardContainer div.bottomV2")
         for home in homes:
             price = home.css("div.bottomV2 span.homecardV2Price::text").get()
@@ -19,10 +25,8 @@ class RentSpider(scrapy.Spider):
             baths = stats[1]
             area = stats[2]
             address = home.css("a div.link-and-anchor::text").get()
-            url = home.css("a").attrib.get("href")
-            part1 = "htt" + "ps://w" + "ww.re"
-            part2 = "dfi" + "n.com"
-            url = part1 + part2 + url
+            home_url = home.css("a").attrib.get("href")
+            home_url = base_url + home_url
 
             yield {
                 "price": price,
@@ -30,5 +34,17 @@ class RentSpider(scrapy.Spider):
                 "baths": baths,
                 "area": area,
                 "address": address,
-                "url": url,
+                "url": home_url,
             }
+
+        page_locator = response.css("div.viewingPage span.pageText::text").re(
+            "Viewing page (\d+) of (\d+)"
+        )
+        current_page = int(page_locator[0])
+        next_page = current_page + 1
+        last_page = int(page_locator[1])
+
+        if current_page < last_page:
+            next_page = f"{base_url}{city_url}/page-{str(next_page)}"
+            print(next_page)
+            yield response.follow(next_page, callback=self.parse)
